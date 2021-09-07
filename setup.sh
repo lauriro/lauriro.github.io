69eb3ca4d12f447d2b2837921ed0db6097c4d4cc
Lauri Rooden <lauri@rooden.ee>
2021-09-07 17:57:52 +0300

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+
+mkdir -p ~/.config ~/.ssh
+
+# Prepare .ssh folder and files
+chmod 700 ~/.ssh
+touch ~/.ssh/authorized_keys ~/.ssh/config ~/.ssh/known_hosts
+chmod 600 ~/.ssh/authorized_keys ~/.ssh/config ~/.ssh/known_hosts
+
+# Generating SSH keys
+ssh-keygen -t rsa -b 2048 -C "lauri@rooden.ee-$(hostname -s)"
+
+cat .ssh/id_rsa.pub
+read -p "Add previous key to github..."
+
+git clone --bare git@github.com:lauriro/dotfiles.git ~/.config/dotfiles.git
+
+export GIT_DIR=~/.config/dotfiles.git
+export GIT_WORK_TREE=~
+git reset --hard
+unset GIT_DIR
+unset GIT_WORK_TREE
+
